7cfd8330aa66324b92ed82d1b6e2e093793b36a9
dusndjnks <mhdaffan0000@gmail.com>
2024-11-18 22:48:21 +0530

    x

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,7 +19,7 @@ const About = () => {
        <div className='px-8'>
      <div className='md:flex-row flex flex-col justify-center h-full xl:gap-20 lg:gap-12 md:gap-12 md:py-20 py-10 items-center'>
      <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
-       <img src={img3} alt="" className=' hover:transition ease-in-out lg:w-96 lg:h-96  md:w-80 md:h-80 w-56 h-56  rounded-xl border-4 drop-shadow-2xl ' />
+       <img src={img3} alt="" className='z-[-1] hover:transition ease-in-out lg:w-96 lg:h-96  md:w-80 md:h-80 w-56 h-56  rounded-xl border-4 drop-shadow-2xl ' />
      </div>
      <div className='flex flex-col items-center justify-center md:pt-0 pt-5 '>
        <div className='flex items-center justify-center gap-4'>
@@ -28,12 +28,12 @@ const About = () => {
              <h1 className='lg:text-2xl md:text-lg text-md'>Experience</h1>
              <h1 className='lg:text-lg md:text-md text-sm'>9 Months</h1>
          </div> 
-         <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 sm:w-18 h-18  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
+         <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 h-20 w-20  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
            <WorkIcon/>
              <h1 className='lg:text-2xl md:text-lg text-md'>Completed</h1>
              <h1 className='lg:text-lg md:text-md text-sm'>4 Projects</h1>
          </div> 
-        <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 sm:w-18 h-18  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
+        <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 w-20 h-20  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
          <PhoneIcon/>
              <h1 className='lg:text-2xl md:text-lg text-md'>Contact</h1>
              <h1 className='lg:text-lg md:text-md text-sm'>7736999627</h1>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,12 +10,12 @@ const Contact = () => {
    <Layout>
      <div className='bg-gray-900 h-screen text-white '>
       <div className='w-screen flex items-center  justify-center'>
-          <h1 className='lg:text-6xl md:text-4xl s:text-3xl text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 md:py-20 py-5 '>Get In Touch</h1>
+          <h1 className='lg:text-6xl md:text-4xl text-3xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 md:py-20 py-5 '>Get In Touch</h1>
         </div>
 
         <div className='flex   lg:px-72 md:px-32 sm:px-24 s:px-18 px-7'>
           <div>
-              <h1 class="lg:text-5xl md:text-4xl sm:text-3xl text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Let's Talk</h1>
+              <h1 class="lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Let's Talk</h1>
               <h1 className='md:w-[600px] md:text-lg text-md py-2'>I am a highly motivated fresher, available for work opportunities. With a strong foundation in JavaScript, HTML and Tailwind CSS, I am eager to learn and contribute to a team.Let's connect!</h1>
               <div className='flex md:gap-3 gap-2  md:text-2xl text-xl items-center py-2'>
                 <EmailIcon/>
