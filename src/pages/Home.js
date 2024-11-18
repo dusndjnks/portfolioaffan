6e1252ae7c7deaa0914b65545e141e13fcf59e85
dusndjnks <mhdaffan0000@gmail.com>
2024-11-18 21:27:05 +0530

    modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,24 +8,20 @@ const Home = () => {
   return (
     <Layout>
         <div>
-        <div className='flex justify-between px-64  h-full py-20 items-center  text-white bg-gray-800'>
+        <div className='md:flex-row justify-between flex flex-col lg:px-28 md:px-14 sm:px-20 px-10 h-full lg:py-20 md:py-14 py-10 items-center  text-white bg-gray-800'>
+                <div>
+                    <img src={img} alt="" className='lg:w-[350px] md:w-[300px] sm:w-[230px] s:w-[250px] xs:w-[200px]  rounded-2xl' />
+                </div>
                 <div className='flex flex-col'>
                     <div className=''>
-                    <h1 className='text-2xl'>Hello I'm</h1>
-                    <h1 className='text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>Muhammed Affan</h1>
-                    <h1 className='text-3xl pb-5'>Software Developer</h1>
+                    <h1 className='md:text-2xl text-lg'>Hello I'm</h1>
+                    <h1 className='lg:text-5xl md:text-4xl s:text-3xl text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>Muhammed Affan</h1>
+                    <h1 className='md:text-3xl text-xl pb-5'>Software Developer</h1>
                     </div>
-                    {/* <h1 className='w-[600px]'>A motivated and hardworking Software Developer with a focus on JavaScript, eager to begin a career in tech. Skilled in JavaScript fundamentals and actively learning frameworks like React and Node.js. Known for problem-solving abilities, a commitment to clean code, and a passion for continuous learning. Excited to contribute to a collaborative team and develop innovative solutions while expanding technical skills.</h1> */}
-                  <div className='py-3'>
-                    <div className='flex'>
-                        <a href={Affan} download="resume" className="download btn text-2xl px-10 py-2 border-2 border-black rounded-full mx-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500" >Download</a>
-                        <Link to="contact" className='text-2xl px-5 py-2 border-2 border-black rounded-full mx-2 bg-black text-white'>Contact Info</Link>
+                    <div className='flex py-3'>
+                        <a href={Affan} download="resume" className="lg:text-2xl md:text-xl text-lg lg:px-6 md:px-4 px-3 py-2 flex items-center justify-center  text-center rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500" >Download</a>
+                        <Link to="contact" className='lg:text-2xl md:text-xl text-lg  lg:px-5 md:px-4 px-2     py-2 border-2 border-black rounded-full mx-2 bg-black text-white'>Contact Info</Link>
                     </div>
-                    
-                  </div>
-                </div>
-                <div>
-                    <img src={img} alt="" className='w-[400px] rounded-2xl' />
                 </div>
             </div>
         </div>
