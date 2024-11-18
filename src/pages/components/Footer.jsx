fed7030f32506b21539a9d2191d3571c6a2a06da
dusndjnks <mhdaffan0000@gmail.com>
2024-11-18 21:47:44 +0530

    x

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-        <div className='flex md:h-24 h-16 bg-gray-200 items-center justify-center'> 
+        <div className='flex h-24  bg-gray-200 items-center justify-center'> 
           <div className='flex   md:gap-20 s:gap-10 gap-5 text-xl  md:text-2xl font-serif'>
           <a href="https://www.instagram.com/a_ffan/profilecard/?igsh=MWM0dm81bXNncWhobA==">Instagram</a>
           <a href="https://www.linkedin.com/in/muhammed-affan-87456724b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Linkedin</a>
