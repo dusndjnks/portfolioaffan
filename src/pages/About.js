0ebd747a5f9150a0a88ee5f190f1adc31c317737
dusndjnks <mhdaffan0000@gmail.com>
2024-11-18 22:30:16 +0530

    x

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,51 +11,48 @@ const About = () => {
    <Layout>
     <div className=' flex flex-col bg-gray-300'>
 
-<div className='flex flex-col items-center justify-center '>
-      <h1 className='text-6xl font-semibold pt-10
-      '>About Me</h1>
-      <h1 className='text-2xl'>My Introdution</h1>
-
+    <div className='flex flex-col items-center justify-center '>
+      <h1 className='lg:text-6xl md:text-5xl  sm:text-4xl text-3xl font-semibold pt-5'>About Me</h1>
+      <h1 className='md:text-2xl text-xl'>My Introdution</h1>
     </div>
 
-<div>
-<div className='flex justify-center gap-40 h-full py-20 items-center'>
-  <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
-    <img src={img3} alt="" className=' hover:transition ease-in-out  w-96 h-96 rounded-xl border-4 drop-shadow-2xl ' />
+       <div className='px-8'>
+     <div className='md:flex-row flex flex-col justify-center h-full xl:gap-20 lg:gap-12 md:gap-12 md:py-20 py-10 items-center'>
+     <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
+       <img src={img3} alt="" className=' hover:transition ease-in-out lg:w-96 lg:h-96  md:w-80 md:h-80 w-56 h-56  rounded-xl border-4 drop-shadow-2xl ' />
+     </div>
+     <div className='flex flex-col items-center justify-center md:pt-0 pt-5 '>
+       <div className='flex items-center justify-center gap-4'>
+         <div className='border-4 drop-shadow-2xl lg:h-32 lg:w-32 md:w-24 md:h-24 w-20 h-20  rounded-xl  bg-gray-100 flex flex-col  items-center justify-center'>
+           <WorkHistoryIcon/>
+             <h1 className='lg:text-2xl md:text-lg text-md'>Experience</h1>
+             <h1 className='lg:text-lg md:text-md text-sm'>9 Months</h1>
+         </div> 
+         <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 sm:w-18 h-18  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
+           <WorkIcon/>
+             <h1 className='lg:text-2xl md:text-lg text-md'>Completed</h1>
+             <h1 className='lg:text-lg md:text-md text-sm'>4 Projects</h1>
+         </div> 
+        <div className='border-4 lg:h-32 lg:w-32 md:w-24 md:h-24 sm:w-18 h-18  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
+         <PhoneIcon/>
+             <h1 className='lg:text-2xl md:text-lg text-md'>Contact</h1>
+             <h1 className='lg:text-lg md:text-md text-sm'>7736999627</h1>
+         </div> 
+       </div >
+         <h1 className='lg:w-[600px] md:w-[300px] pt-10 text-center'>Hardworking Software Developer focused on JavaScript, with a strong foundation and a commitment to continuous learning. Actively building skills in React and Node.js. Excited to bring a fresh perspective, problem-solving mindset, and enthusiasm for growth to a team.</h1>
+     </div>
+    </div>
   </div>
-  <div className='flex flex-col'>
-    <div className='flex gap-4'>
-      <div className='border-4 drop-shadow-2xl h-40 w-40  rounded-xl  bg-gray-100 flex flex-col  items-center justify-center'>
-        <WorkHistoryIcon/>
-          <h1 className='text-2xl'>Experience</h1>
-          <h1 className='text-lg'>9 Months</h1>
-      </div>
-      <div className='border-4 h-40 w-40  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
-        <WorkIcon/>
-          <h1 className='text-2xl'>Completed</h1>
-          <h1 className='text-lg'>4 Projects</h1>
-      </div>
-     <div className='border-4 h-40 w-40  rounded-xl drop-shadow-2xl bg-gray-100 flex flex-col  items-center justify-center'>
-      <PhoneIcon/>
-          <h1 className='text-2xl'>Contact</h1>
-          <h1 className='text-lg'>7736999627</h1>
-      </div>
-     
-    </div >
-      <h1 className='w-[600px] pt-10'>Hardworking Software Developer focused on JavaScript, with a strong foundation and a commitment to continuous learning. Actively building skills in React and Node.js. Excited to bring a fresh perspective, problem-solving mindset, and enthusiasm for growth to a team.</h1>
-  </div>
-</div>
-</div>
 
 
   <div className='flex flex-col items-center justify-center '>
-      <h1 className='text-6xl font-semibold'>Skills</h1>
-      <h1 className='text-2xl'>My Technical Level</h1>
+      <h1 className='lg:text-6xl md:text-4xl sm:text-3xl text-2xl font-semibold'>Skills</h1>
+      <h1 className='md:text-2xl text-xl'>My Technical Level</h1>
   </div>
 
 
 
-   <div className='flex justify-between px-72 py-20 '>
+   {/* <div className='flex justify-between px-72 py-20 '>
    <div className=' gap-9 border-4  rounded-xl px-16 py-7 drop-shadow-2xl bg-gray-100 flex flex-col items-center ' >
     <h1 className='text-3xl font-bold underline'>Frontend Skill</h1>
             <div className='flex gap-10'>
@@ -118,7 +115,7 @@ const About = () => {
             </div>
             </div>
     </div>
-   </div>
+   </div> */}
 
 </div>
    </Layout>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,7 @@ import Affan from "../image/Affan.CV.2024.pdf"
 const Home = () => {
   return (
     <Layout>
-        <div>
-        <div className='md:flex-row justify-between flex flex-col lg:px-28 md:px-14 sm:px-20 px-10 h-full lg:py-20 md:py-14 py-10 items-center  text-white bg-gray-800 gap-10'>
+        <div className='md:flex-row justify-between flex flex-col 2xl:px-72 xl:px-56 lg:px-32 md:px-14 sm:px-20 px-10 h-full lg:py-20 md:py-14 py-10 items-center  text-white bg-gray-800 gap-10'>
                 <div>
                     <img src={img} alt="" className='lg:w-[350px] md:w-[300px] sm:w-[230px] s:w-[250px] xs:w-[200px]  rounded-2xl' />
                 </div>
@@ -24,7 +23,6 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </div>
     </Layout>
   )
 }
