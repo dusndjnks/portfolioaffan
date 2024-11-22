535a5baf61087ef7464a50cc87bc0be8ab95a90e
dusndjnks <mhdaffan0000@gmail.com>
2024-11-22 21:42:11 +0530

    x

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,63 +16,63 @@ const Projects = () => {
             </div>
 
     <div className='2xl:px-64 xl:px-56 lg:px- md:px-20 sm:px-16 s:px-12 px-6  flex flex-col gap-14 pb-20'>
-          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
+          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-18 lg:gap-8 md:gap-14 sm:gap-8 gap-4 l items-center'>
             <div>
               <h1 className='lg:text-lg md:text-md text-sm text-gray-600'>UI/UX Design</h1>
               <h className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-blue-600'>VS Code Clone</h>
-              <h1 className='text-lg text-blue-800'>JavaScript,HTML,Tailwind CSS,Github,React</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-blue-800'>JavaScript,HTML,Tailwind CSS,Github,React</h1>
             </div>
             <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
               <a href="https://mhd-vscode-clone.netlify.app/">
-              <img src={pp3} alt="" className='lg:h-[350px] md: bg-blue-500 p-2' />
+              <img src={pp3} alt="" className='lg:h-[350px]  bg-blue-500 p-2' />
               </a>
             </div>
           </div>
 
-        {/* <div className=''>
-          <div className='flex justify-center gap-32 items-center'>
+        <div className=''>
+          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-18 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
           <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
             <a href="https://dusndjnks.github.io/RouterDom/">
-            <img src={pp2} alt="" className='h-[350px] bg-yellow-300 p-2' />
+            <img src={pp2} alt="" className='lg:h-[350px] bg-yellow-300 p-2' />
             </a>
             </div>
             <div>
-              <h1 className='text-lg text-yellow-600'>Ecommerce Site</h1>
-              <h1 className='text-5xl underline text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-600'>Dress Store</h1>
-              <h1 className='text-lg text-green-800'>JavaScript,HTML,Tailwind CSS,Github,React(Full Stack)</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-yellow-600'>Ecommerce Site</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-600'>Dress Store</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-green-800'>JavaScript,HTML,Tailwind CSS,Github,React(Full Stack)</h1>
             </div>
           </div>
         </div>
      
         <div className=''>
-          <div className='flex justify-center gap-32 items-center'>
+          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
             <div>
-              <h1 className='text-lg text-red-800'>Ecommerce Site</h1>
-              <h1 className='text-5xl underline text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-800'>Watch Store</h1>
-              <h1 className='text-lg text-orange-800'>JavaScript,HTML,Tailwind CSS,React,Github,Netlify(Nested Route)</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-red-800'>Ecommerce Site</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline    font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-800'>Watch Store</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-orange-800'>JavaScript,HTML,Tailwind CSS,React,Github,Netlify(Nested Route)</h1>
             </div>
             <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
               <a href="https://mhdpatekphilippe.netlify.app/">
-              <img src={pp1} alt="" className='h-[350px] bg-orange-300 p-2' />
+              <img src={pp1} alt="" className='lg:h-[350px] bg-orange-300 p-2' />
               </a>
             </div>
           </div>
         </div>
 
         <div className=''>
-          <div className='flex justify-center gap-32 items-center'>
+          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
           <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
             <a href="https://mhd-plant-homepage.netlify.app/">
-            <img src={pp4} alt="" className='h-[350px] bg-green-500 p-2' />
+            <img src={pp4} alt="" className='lg:h-[350px] bg-green-500 p-2' />
             </a>
             </div>
             <div className='pr-10'>
-              <h1 className='text-lg text-blue-600'>UI/UX Design</h1>
-              <h1 className='text-5xl underline text-5xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-800'>Plant Store</h1>
-              <h1 className='text-lg text-green-800'>JavaScript,HTML,Tailwind CSS,Github</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-blue-600'>UI/UX Design</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-800'>Plant Store</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-green-800'>JavaScript,HTML,Tailwind CSS,Github</h1>
             </div>
           </div>
-        </div> */}
+        </div>
 
     </div>
 </div>
