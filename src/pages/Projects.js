e6cbcdc1c02fb461d137bab26fa1fd2ce4f365ec
dusndjnks <mhdaffan0000@gmail.com>
2024-11-25 22:03:28 +0530

    modified

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,7 @@ import pp1 from "../image/pp1.png"
 import pp2 from "../image/pp2.png"
 import pp3 from "../image/pp3.png"
 import pp4 from "../image/pp4.png"
+import pp5 from "../image/pp5.jpg"
 
 
 const Projects = () => {
@@ -12,14 +13,31 @@ const Projects = () => {
       <hr className='border-2 border-black'/>
 <div className='bg-gray-300'>
             <div className='w-screen   flex items-center justify-center  '>
-                <h1 className='lg:text-6xl md:text-5xl text-4xl  font-semibold pt-5 pb-10 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  '>Projects</h1>
+                <h1 className='lg:text-6xl md:text-5xl text-4xl  font-semibold pt-5 pb-10 font-extrabold  '>Projects</h1>
             </div>
 
     <div className='2xl:px-64 xl:px-56 lg:px- md:px-20 sm:px-16 s:px-12 px-4  flex flex-col gap-20 pb-20 '>
+
+    <div className=''>
+          <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
+          <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
+            <a href="https://mhd-plant-homepage.netlify.app/">
+            <img src={pp5} alt="" className='lg:h-[350px] bg-purple-600 p-2' />
+            </a>
+            </div>
+            <div className='pr-10'>
+              <h1 className='lg:text-lg md:text-md text-sm text-blue-600'>UI/UX Design</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>PORTFOLIO</h1>
+              <h1 className='lg:text-lg md:text-md text-sm text-pink-600'>JavaScript,HTML,Tailwind CSS,Github</h1>
+            </div>
+          </div>
+        </div>
+
+
           <div className='md:flex-row flex flex-col-reverse	 justify-center 2xl:gap-32 xl:gap-18 lg:gap-8 md:gap-14 sm:gap-8 gap-4 l items-center'>
             <div>
               <h1 className='lg:text-lg md:text-md text-sm text-gray-600'>UI/UX Design</h1>
-              <h className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-blue-600'>VS Code Clone</h>
+              <h className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-blue-600 '>VS Code Clone</h>
               <h1 className='lg:text-lg md:text-md text-sm text-blue-800'>JavaScript,HTML,Tailwind CSS,Github,React</h1>
             </div>
             <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
@@ -28,6 +46,8 @@ const Projects = () => {
               </a>
             </div>
           </div>
+
+
 
         <div className=''>
           <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-18 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
@@ -38,17 +58,16 @@ const Projects = () => {
             </div>
             <div>
               <h1 className='lg:text-lg md:text-md text-sm text-yellow-600'>Ecommerce Site</h1>
-              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-600'>Dress Store</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-green-600'>LEGACY ELEGANCE</h1>
               <h1 className='lg:text-lg md:text-md text-sm text-green-800'>JavaScript,HTML,Tailwind CSS,Github,React(Full Stack)</h1>
             </div>
           </div>
         </div>
      
-        <div className=''>
           <div className='md:flex-row flex flex-col-reverse justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
             <div>
               <h1 className='lg:text-lg md:text-md text-sm text-red-800'>Ecommerce Site</h1>
-              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline    font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-800'>Watch Store</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline    font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-800'>PATEK PHILIPPE</h1>
               <h1 className='lg:text-lg md:text-md text-sm text-orange-800'>JavaScript,HTML,Tailwind CSS,React,Github,Netlify(Nested Route)</h1>
             </div>
             <div className='transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110'>
@@ -57,7 +76,6 @@ const Projects = () => {
               </a>
             </div>
           </div>
-        </div>
 
         <div className=''>
           <div className='md:flex-row flex flex-col justify-center 2xl:gap-32 xl:gap-28 lg:gap-16 md:gap-14 sm:gap-8 gap-4 l items-center'>
@@ -68,11 +86,15 @@ const Projects = () => {
             </div>
             <div className='pr-10'>
               <h1 className='lg:text-lg md:text-md text-sm text-blue-600'>UI/UX Design</h1>
-              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-800'>Plant Store</h1>
+              <h1 className='lg:text-6xl md:text-5xl sm:text-4xl text-3xl underline  font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-800'>MONSTERA</h1>
               <h1 className='lg:text-lg md:text-md text-sm text-green-800'>JavaScript,HTML,Tailwind CSS,Github</h1>
             </div>
           </div>
         </div>
+
+
+        
+
     </div>
 </div>
 
