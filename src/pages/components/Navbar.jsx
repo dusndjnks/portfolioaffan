295e070a961ee035c088cbfade2363023da7deb6
dusndjnks <mhdaffan0000@gmail.com>
2025-07-03 22:50:37 +0530

    x

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <header className="bg-gray-200 font-serif z-50 w-full shadow-md sticky top-0">
-      <nav className="max-w-7xl mx-auto px-6 sm:px-12 md:px-20 flex justify-between items-center h-24">
+      <nav className="max-w-7xl mx-auto px-6 sm:px-12 md:px-20 flex justify-between items-center md:h-24 h-20">
         {/* Logo */}
         <Link to="/" className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-800">
           Affan
